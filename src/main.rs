9e073e86f03f4266fa7c6f98ce9973315ebf7f4b
Christopher Parker <hectorgrey223@protonmail.com>
2023-07-29 05:07:07 +0100

    added additional page templates

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,25 @@ fn index() -> Template {
     Template::render("index", context!())
 }
 
+#[get("/rust")]
+fn rust() -> Template {
+    Template::render("rust", context!())
+}
+
+#[get("/linux")]
+fn linux() -> Template {
+    Template::render("linux", context!())
+}
+
+#[get("/freebsd")]
+fn freebsd() -> Template {
+    Template::render("freebsd", context!())
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Template::fairing())
-        .mount("/", routes![index])
+        .mount("/", routes![index, rust, linux, freebsd])
         .mount("/", FileServer::from(relative!("static")))
 }
