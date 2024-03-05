1e0f02cbb95579ff640cc41c499e844a4ff7f3f6
hectorgrey <parker321@gmail.com>
2024-03-05 07:12:24 +0000

    changed formatting to use tabs

--- src/article.rs
+++ src/article.rs
@@ -1,22 +1,22 @@
 use std::fmt::Display;
 
 pub enum ArticleCategory {
-    Rust,
-    Linux,
+	Rust,
+	Linux,
 }
 
 impl Display for ArticleCategory {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ArticleCategory::Rust => write!(f, "Rust"),
-            ArticleCategory::Linux => write!(f, "Linux"),
-        }
-    }
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			ArticleCategory::Rust => write!(f, "Rust"),
+			ArticleCategory::Linux => write!(f, "Linux"),
+		}
+	}
 }
 
 pub struct Article {
-    id: u32,
-    title: String,
-    content: String,
-    category: ArticleCategory,
+	id: u32,
+	title: String,
+	content: String,
+	category: ArticleCategory,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,36 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket::fs::{
-    relative,
-    FileServer,
-};
+use rocket::fs::{relative, FileServer};
 
-use rocket_dyn_templates::{
-    Template,
-    context,
-};
+use rocket_dyn_templates::{context, Template};
 
 mod article;
 
 #[get("/")]
 fn index() -> Template {
-    Template::render("index", context!())
+	Template::render("index", context!())
 }
 
 #[get("/articles/<article_index>")]
 fn display_article(article_index: usize) -> Template {
-    Template::render("article", context!(index: article_index))
+	Template::render("article", context!(index: article_index))
 }
 
 #[get("/rust")]
 fn rust() -> Template {
-    Template::render("rust", context!())
+	Template::render("rust", context!())
 }
 
 #[get("/linux")]
 fn linux() -> Template {
-    Template::render("linux", context!())
+	Template::render("linux", context!())
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .attach(Template::fairing())
-        .mount("/", routes![index, rust, linux, display_article])
-        .mount("/", FileServer::from(relative!("static")))
+	rocket::build()
+		.attach(Template::fairing())
+		.mount("/", routes![index, rust, linux, display_article])
+		.mount("/", FileServer::from(relative!("static")))
 }
