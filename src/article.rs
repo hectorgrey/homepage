b6c88cd1286a30dd0723baa5135bc575adb5c6b8
hectorgrey <hectorgrey223@protonmail.com>
2025-07-11 20:17:54 +0100

    Now able to load articles

--- src/article.rs
+++ src/article.rs
@@ -1,9 +1,12 @@
 use std::fmt::Display;
 
+use rocket_db_pools::{sqlx, Connection};
+
 pub enum ArticleCategory {
 	Rust,
 	Linux,
 	FreeBSD,
+	Other,
 }
 
 impl Display for ArticleCategory {
@@ -12,13 +15,44 @@ impl Display for ArticleCategory {
 			ArticleCategory::Rust => write!(f, "Rust"),
 			ArticleCategory::Linux => write!(f, "Linux"),
 			ArticleCategory::FreeBSD => write!(f, "FreeBSD"),
+			ArticleCategory::Other => write!(f, "Other"),
+		}
+	}
+}
+
+impl From<String> for ArticleCategory {
+	fn from(value: String) -> Self {
+		match value.as_str() {
+			"Rust" => ArticleCategory::Rust,
+			"Linux" => ArticleCategory::Linux,
+			"FreeBSD" => ArticleCategory::FreeBSD,
+			_ => ArticleCategory::Other,
 		}
 	}
 }
 
 pub struct Article {
-	id: i64,
-	title: String,
-	content: String,
-	category: ArticleCategory,
+	pub title: String,
+	pub content: String,
+	pub category: ArticleCategory,
+}
+
+impl Article {
+	pub async fn read(mut db: Connection<crate::Blog>, id: i64) -> Option<Article> {
+		let row: Result<Option<(String, String, String)>, sqlx::Error> =
+			sqlx::query_as("select title, content, category from articles where id = ?")
+				.bind(id)
+				.fetch_optional(&mut **db)
+				.await;
+		if let Ok(row) = row {
+			if let Some((title, content, category)) = row {
+				return Some(Article {
+					title,
+					content,
+					category: ArticleCategory::from(category),
+				});
+			}
+		}
+		None
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate rocket;
 
 use rocket::fs::{relative, FileServer};
 
-use rocket_db_pools::{sqlx, Database};
+use rocket_db_pools::{sqlx, Connection, Database};
 use rocket_dyn_templates::{context, Template};
 
 mod article;
@@ -18,8 +18,19 @@ fn index() -> Template {
 }
 
 #[get("/articles/<article_index>")]
-fn display_article(article_index: usize) -> Template {
-	Template::render("article", context!(index: article_index))
+async fn display_article(db: Connection<Blog>, article_index: i64) -> Template {
+	let article = article::Article::read(db, article_index).await;
+	if let Some(article) = article {
+		Template::render(
+			"article",
+			context!(
+				title: article.title,
+				content: article.content,
+				category: article.category.to_string()),
+		)
+	} else {
+		Template::render("article_not_found", context!())
+	}
 }
 
 #[get("/rust")]
